24fab3e905fbdb7d15b9081d73319819d1ba5dd4
KennyHisano <nobi3892@gmail.com>
2018-02-07 09:51:23 +0900

    initiate cmd arg parse

--- cmd/btcatomic/main.go
+++ cmd/btcatomic/main.go
@@ -5,15 +5,42 @@ import(
 	"strings"
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
+	"strconv"
+	"net/rpc"
 )
 
 func main() {
 
 }
 
-func run(){
+type command interface {
+	runcommand(*rpc.Client) error
+}
 
+type initiateCmd struct {
+	cp2Addr *btcutil.AddressPubKeyHash
+	amount  btcutil.Amount
+}
+
+
+
+func run() (err error) {
+
+	var cmd command
+	//get address of testnet
+
+	var s = "senderaddresshere"
+	amountVal := float64(1.3)
 	var chainparams = &chaincfg.TestNet3Params
-	s = "a"
 	cp2Addr, err := btcutil.DecodeAddress(s,chainparams)
-	
+
+	amount, err := btcutil.NewAmount(amountVal)
+	if err != nil {
+			return fmt.Errorf("amount wrong")
+	}
+
+	cp2AddrP2PKH := cp2Addr.(*btcutil.AddressPubKeyHash)
+
+	cmd = &initiateCmd{cp2Addr: cp2AddrP2PKH,amount: amount}
+
+}
